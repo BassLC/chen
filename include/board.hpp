7d8cbd7380c5b889404feed40e0c0b6b2cf0c72f
BassLC <bernardolimaconde@gmail.com>
2018-08-01 16:06:02 +0100

    Added board pretty printing.

--- include/board.hpp
+++ include/board.hpp
@@ -1,36 +1,33 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
-#include <unordered_map>
 #include <cstdint>
+#include <unordered_map>
 
+using bitboard = uint64_t;
 
-// A mapping of array indexes
-// to recognizable names
-enum class Piece {
-    Pawns,
-    Bishops,
-    Knights,
-    Rooks,
-    Queens,
-    King,
-}; 
+enum class Piece : char{
+    Pawns = 'p',
+    Bishops = 'b',
+    Knights = 'n',
+    Rooks = 'r',
+    Queens = 'q',
+    King = 'k',
+};
 
 
 class Board {
 
-public:
+  public:
     Board();
     ~Board() = default;
-    uint64_t all_white_pieces();
-    uint64_t all_black_pieces();
+    bitboard get_all_white_pieces();
+    bitboard get_all_black_pieces();
     void pretty_print();
-        
-private:
-    std::unordered_map<Piece, uint64_t> m_white_pieces;
-    std::unordered_map<Piece, uint64_t> m_black_pieces;
+
+  private:
+    std::unordered_map<Piece, bitboard> m_white_pieces;
+    std::unordered_map<Piece, bitboard> m_black_pieces;
 };
-
-
 
 #endif
--- src/board.cpp
+++ src/board.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
 #include "../include/board.hpp"
+#include <iostream>
 
-
-// It initializes the board position
-// to the normal starting postion
+// @TODO:
+// - Make a constructor that allows to load a specific configuration (maybe from
+// a PGN or FEN).
 Board::Board() {
-    
     // Allocate all the space necessary in one go
     m_white_pieces.reserve(6);
     m_black_pieces.reserve(6);
@@ -24,7 +23,6 @@ Board::Board() {
     m_white_pieces[Piece::King] =
         0b0000000000000000000000000000000000000000000000000000000000001000;
 
-
     m_black_pieces[Piece::Pawns] =
         0b0000000011111111000000000000000000000000000000000000000000000000;
     m_black_pieces[Piece::Bishops] =
@@ -39,35 +37,55 @@ Board::Board() {
         0b0000100000000000000000000000000000000000000000000000000000000000;
 }
 
-
-uint64_t Board::all_white_pieces() {
-    return (m_white_pieces[Piece::Pawns] |
-            m_white_pieces[Piece::Bishops] |
-            m_white_pieces[Piece::Knights] |
-            m_white_pieces[Piece::Rooks] |
-            m_white_pieces[Piece::Queens] |
-            m_white_pieces[Piece::King]);
+bitboard Board::get_all_white_pieces() {
+    return (m_white_pieces[Piece::Pawns] | m_white_pieces[Piece::Bishops] |
+            m_white_pieces[Piece::Knights] | m_white_pieces[Piece::Rooks] |
+            m_white_pieces[Piece::Queens] | m_white_pieces[Piece::King]);
 }
 
-
-uint64_t Board::all_black_pieces() {
-    return (m_black_pieces[Piece::Pawns] |
-            m_black_pieces[Piece::Bishops] |
-            m_black_pieces[Piece::Knights] |
-            m_black_pieces[Piece::Rooks] |
-            m_black_pieces[Piece::Queens] |
-            m_black_pieces[Piece::King]);
-    
+bitboard Board::get_all_black_pieces() {
+    return (m_black_pieces[Piece::Pawns] | m_black_pieces[Piece::Bishops] |
+            m_black_pieces[Piece::Knights] | m_black_pieces[Piece::Rooks] |
+            m_black_pieces[Piece::Queens] | m_black_pieces[Piece::King]);
 }
-
 
 void Board::pretty_print() {
-    uint64_t b = Board::all_white_pieces() | Board::all_black_pieces();
-    for ( int i = 0; i < 64; ++i) {
-        std::cout << static_cast<int>(b & 0b1);
-        b >>= 1;
+    std::string board(64, '.');
+    const std::array<Piece, 6> piece_type{
+        Piece::Pawns, Piece::Bishops, Piece::Knights,
+        Piece::Rooks, Piece::Queens,  Piece::King,
+    };
 
-        if ( (i+1) % 8 == 0 ) {
+    // @TODO:
+    // - Get rid of this code duplication.
+    // ? Make this function return a string, instead of printing info.
+
+    // White pieces
+    for (const auto p : piece_type) {
+        bitboard b = m_white_pieces[p];
+        for (auto i = 63; i >= 0; --i) {
+            if (b & 0b1) {
+                board[i] = std::toupper(static_cast<char>(p));
+            }
+            b >>= 1;
+        }
+    }
+
+    // Black pieces
+    for (const auto p : piece_type) {
+        bitboard b = m_black_pieces[p];
+        for (auto i = 63; i >= 0; --i) {
+            if (b & 0b1) {
+                board[i] = static_cast<char>(p);
+            }
+            b >>= 1;
+        }
+    }
+
+    // Print it (and put spaces in each line)
+    for (auto i = 0; i < 64; ++i) {
+        std::cout << board[i];
+        if ((i + 1) % 8 == 0) {
             std::cout << '\n';
         }
     }
