1265932c60cbc666b29192e95cd88152cf4ffaab
BassLC <bernardolimaconde@gmail.com>
2018-08-13 20:05:41 +0100

    Changed to_square function to use DeBruijn Multiplication instead of the naive iteration.

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -1,6 +1,5 @@
 #include "../include/movegen.hpp"
 #include <cassert>
-#include <iostream> // @TODO: Remove this header
 
 namespace File {
 constexpr bitboard A = 0x8080808080808080;
@@ -30,18 +29,22 @@ bitboard get_knight_moves_board(const bitboard& knights_board,
             ~side_board); // Two pieces can't be in the same square
 }
 
-Square to_square(bitboard b) {
+square to_square(bitboard b) {
     // To see if there is only one bit set in the argument
-    assert((b && !(b & (b - 1))) == 0);
+    // (b has to be a power of two)
+    assert((b && !(b & (b - 1))) == 1);
 
-    for (uint8_t i = 0; i < 64; ++i) {
-        if ((b & 0b1) == 1) {
-            return i;
-        }
-        b >>= 1;
-    }
+    // Using a DeBruijn Multiplication with an isolated LS1B
+    // For more info:
+    // http://chessprogramming.wikispaces.com/BitScan#Bitscan%20forward-De%20Bruijn%20Multiplication-With%20isolated%20LS1B
+    const square index64[64] = {
+        0,  1,  48, 2,  57, 49, 28, 3,  61, 58, 50, 42, 38, 29, 17, 4,
+        62, 55, 59, 36, 53, 51, 43, 22, 45, 39, 33, 30, 24, 18, 12, 5,
+        63, 47, 56, 27, 60, 41, 37, 16, 54, 35, 52, 21, 44, 32, 23, 11,
+        46, 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9,  13, 8,  7,  6};
 
-    return 0;
+    const bitboard debruijn64 = 0x03f79d71b4cb0a89;
+    return index64[((b & -b) * debruijn64) >> 58];
 }
 
 bitboard get_lsb(const bitboard& b) { return b & (-b); }
@@ -49,6 +52,7 @@ void clear_lsb(bitboard& b) { b &= (b - 1); }
 
 std::vector<Move> MoveGen::generate_knight_moves(Board& board,
                                                  const Color& side) {
+    // @TODO: Make the function work for the Black side
     std::vector<Move> possible_moves;
     for (bitboard all_knights_pos = board.get_piece_board(Piece::Knight, side);
          all_knights_pos != 0; clear_lsb(all_knights_pos)) {
