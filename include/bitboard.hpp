97a0e63dfdf67142bf0b4c50e7eed1ccaec8c643
BassLC <bernardolimaconde@gmail.com>
2018-08-26 17:18:03 +0100

    Added the reverse function and File/Rank constants.

--- include/bitboard.hpp
+++ include/bitboard.hpp
@@ -8,19 +8,31 @@ using square = uint8_t;
 namespace File {
 constexpr bitboard A = 0x8080808080808080;
 constexpr bitboard B = 0x4040404040404040;
-// constexpr bitboard C = 0x2020202020202020;
-// constexpr bitboard D = 0x1010101010101010;
-// constexpr bitboard E = 0x0808080808080808;
-// constexpr bitboard F = 0x0404040404040404;
+constexpr bitboard C = 0x2020202020202020;
+constexpr bitboard D = 0x1010101010101010;
+constexpr bitboard E = 0x0808080808080808;
+constexpr bitboard F = 0x0404040404040404;
 constexpr bitboard G = 0x0202020202020202;
 constexpr bitboard H = 0x0101010101010101;
 } // namespace File
+
+namespace Rank {
+constexpr bitboard ONE = 0xFF;
+constexpr bitboard TWO = 0xFF00;
+constexpr bitboard THREE = 0xFF0000;
+constexpr bitboard FOUR = 0xFF000000;
+constexpr bitboard FIVE = 0xFF00000000;
+constexpr bitboard SIX = 0xFF0000000000;
+constexpr bitboard SEVEN = 0xFF000000000000;
+constexpr bitboard EIGHT = 0xFF00000000000000;
+} // namespace Rank
 
 namespace BitBoard {
 bitboard pop_lsb(bitboard& b);
 bitboard pop_msb(bitboard& b);
 void print(bitboard b);
 square to_square(const bitboard& b);
+bitboard reverse(bitboard b);
 } // namespace BitBoard
 
 #endif
--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,15 +1,15 @@
 #include "../include/bitboard.hpp"
-#include <iostream>
-#include <cassert>
 #include <array>
+#include <cassert>
+#include <iostream>
 
-bitboard BitBoard::pop_lsb(bitboard &b) {
+bitboard BitBoard::pop_lsb(bitboard& b) {
     bitboard lsb = b & -b;
     b &= (b - 1);
     return lsb;
 }
 
-bitboard BitBoard::pop_msb(bitboard &b) {
+bitboard BitBoard::pop_msb(bitboard& b) {
     bitboard msb = b;
     msb |= msb >> 32;
     msb |= msb >> 16;
@@ -25,7 +25,7 @@ bitboard BitBoard::pop_msb(bitboard &b) {
 void BitBoard::print(bitboard b) {
     for (int i = 0; i < 64; ++i) {
         std::cout << ((b & 0x8000000000000000) >> 63);
-        if ((i+1) % 8 == 0) {
+        if ((i + 1) % 8 == 0) {
             std::cout << '\n';
         }
         b <<= 1;
@@ -44,9 +44,26 @@ square BitBoard::to_square(const bitboard& b) {
         0,  1,  48, 2,  57, 49, 28, 3,  61, 58, 50, 42, 38, 29, 17, 4,
         62, 55, 59, 36, 53, 51, 43, 22, 45, 39, 33, 30, 24, 18, 12, 5,
         63, 47, 56, 27, 60, 41, 37, 16, 54, 35, 52, 21, 44, 32, 23, 11,
-        46, 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9,  13, 8,  7,  6
-    };
+        46, 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9,  13, 8,  7,  6};
 
     const bitboard debruijn64 = 0x03f79d71b4cb0a89;
     return index64[((b & -b) * debruijn64) >> 58];
+}
+
+bitboard BitBoard::reverse(bitboard b) {
+    // Reverses a bitboard by 180 degrees (a1 is mapped to h8)
+    // For more information see:
+    // https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating#Rotating
+    const bitboard h1 = 0x5555555555555555;
+    const bitboard h2 = 0x3333333333333333;
+    const bitboard h4 = 0x0F0F0F0F0F0F0F0F;
+    const bitboard v1 = 0x00FF00FF00FF00FF;
+    const bitboard v2 = 0x0000FFFF0000FFFF;
+    b = ((b >> 1) & h1) | ((b & h1) << 1);
+    b = ((b >> 2) & h2) | ((b & h2) << 2);
+    b = ((b >> 4) & h4) | ((b & h4) << 4);
+    b = ((b >> 8) & v1) | ((b & v1) << 8);
+    b = ((b >> 16) & v2) | ((b & v2) << 16);
+    b = (b >> 32) | (b << 32);
+    return b;
 }
